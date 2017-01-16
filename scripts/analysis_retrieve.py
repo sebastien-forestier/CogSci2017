258c6a422095ae54ae3322d3d6e66a1beda149d9
Sébastien Forestier <sebastien.forestier@inria.fr>
2017-01-16 10:04:09 +0100

    analysis

--- scripts/analysis_retrieve.py
+++ scripts/analysis_retrieve.py
@@ -15,18 +15,27 @@ n_iter = 100
 # RETRIEVE LOGS
 trial_list = range(1,n_iter + 1) 
 data_vocal = {}
+data_competence = {}
 
 for config_name in config_list:
     data_vocal[config_name] = {}
+    data_competence[config_name] = {}
     for trial in trial_list:
         data_vocal[config_name][trial] = {}
+        data_competence[config_name][trial] = {}
         filename = log_dir + '/pickle/log-{}-{}'.format(config_name, trial) + '.pickle'
         with open(filename, 'r') as f:
             log = cPickle.load(f)
         f.close()
+        
+        # VOCAL
         data_vocal[config_name][trial]["errors"] = log["environment"]["best_vocal_errors_evolution"]
         data_vocal[config_name][trial]["human_sounds"] = log["environment"]["human_sounds"]
-
+        
+        # COMPETENCE
+        data_competence[config_name][trial]["eval_results"] = log["eval_results"]
+        
+        
 # DUMP RESULT
 if not os.path.exists(log_dir + "/results"):
     os.mkdir(log_dir + "/results")
@@ -36,3 +45,7 @@ with open(filename, 'wb') as f:
     cPickle.dump(data_vocal, f)
 f.close()
             
+filename = log_dir + '/results/competence.pickle'
+with open(filename, 'wb') as f:
+    cPickle.dump(data_competence, f)
+f.close()
