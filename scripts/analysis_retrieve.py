785906943be0eabde3e138e1d327cc0683e5e881
Sébastien Forestier <sebastien.forestier@inria.fr>
2017-01-16 18:16:54 +0100

    analysis

--- scripts/analysis_retrieve.py
+++ scripts/analysis_retrieve.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 # PARAMS
-log_dir = "/scratch/sforestier001/logs/CogSci2017/2017-01-16_09-55-15-COGSCI"
+log_dir = "/scratch/sforestier001/logs/CogSci2017/2017-01-16_12-32-59-COGSCI"
 config_list = ["RMB", "AMB"]
 n_iter = 100
 
