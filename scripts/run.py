299839b70e167c9cb44831e2837b362d17b1bd0b
Sébastien Forestier <sebastien.forestier@inria.fr>
2017-01-17 10:44:08 +0100

    proba imitate 0.5

--- scripts/run.py
+++ scripts/run.py
@@ -24,7 +24,7 @@ def run(log_dir, config_name, trial):
         model_babbling = "random"
         n_motor_babbling = 1000
         explo_noise = 0.05
-        proba_imitate = 0.2
+        proba_imitate = 0.5
         gui=False
         audio=False
         
@@ -34,7 +34,7 @@ def run(log_dir, config_name, trial):
         model_babbling = "active"
         n_motor_babbling = 1000
         explo_noise = 0.05
-        proba_imitate = 0.2
+        proba_imitate = 0.5
         gui=False
         audio=False
         
